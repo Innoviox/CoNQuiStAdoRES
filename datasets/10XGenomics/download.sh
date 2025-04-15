bb3b515415bd81ee5e7801cde44ce7f778dd9474
Simon Chervenak <sarras305@gmail.com>
2025-04-15 11:43:09 -0400

    start with 10x dataset

---
+++ datasets/10XGenomics/download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Input Files
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_fastqs.tar
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_target_panel.csv
+
+# Output Files
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_possorted_genome_bam.bam
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_possorted_genome_bam.bam.bai
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_molecule_info.h5
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_filtered_feature_bc_matrix.h5
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_filtered_feature_bc_matrix.tar.gz
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_raw_feature_bc_matrix.h5
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_raw_feature_bc_matrix.tar.gz
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_analysis.tar.gz
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_metrics_summary.csv
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_web_summary.html
+wget https://cf.10xgenomics.com/samples/cell-exp/4.0.0/Targeted_SC3v3_Human_Glioblastoma_Neuroscience/Targeted_SC3v3_Human_Glioblastoma_Neuroscience_cloupe.cloupe
